dd63dda5a70209b3a6db6bda4ecb2329e78988d9
Benjamin Stefan <145866889+Benjamin-Stefan@users.noreply.github.com>
2024-09-27 17:06:19 +0200

    chore: Init

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import tsParser from "@typescript-eslint/parser";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import prettier from "eslint-plugin-prettier";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default [
+    {
+        files: ["src/**/*.{js,mjs,cjs,ts,tsx}"],
+        languageOptions: {
+            parser: tsParser,
+            parserOptions: {
+                ecmaVersion: "latest",
+                sourceType: "module",
+                project: path.resolve(__dirname, "tsconfig.json"),
+                tsconfigRootDir: __dirname,
+            },
+        },
+        plugins: {
+            "@typescript-eslint": tseslint,
+            prettier,
+        },
+        rules: {
+            ...tseslint.configs["recommended-type-checked"].rules,
+            ...eslintConfigPrettier.rules,
+            "prettier/prettier": "error",
+        },
+    },
+];
